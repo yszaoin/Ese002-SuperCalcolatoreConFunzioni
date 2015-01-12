cb96cfea2362fdcb00f2cfce40873068c7e2e954
frtrotta <frtrotta@FOLO>
2015-01-12 10:54:30 +0100

    Soluzione professore

--- main.c
+++ main.c
@@ -79,36 +79,50 @@ int main(int argc, char** argv) {
 }
 
 float somma(float a, float b) {
-    // TODO Definire il corpo della funzione
-    return 0;
+    float risultato;
+    risultato = a+b;
+    return risultato;
 }
 
 float sottrazione(float a, float b) {
-    // TODO Definire il corpo della funzione
-    return 0;
+    float risultato;
+    risultato = a-b;
+    return risultato;
 }
 
 float prodotto(float a, float b) {
-    // TODO Definire il corpo della funzione
-    return 0;
+    float risultato;
+    risultato = a*b;
+    return risultato;
 }
 
 float divisione(float a, float b) {
-    // TODO Definire il corpo della funzione
-    return 0;
+    float risultato;
+    risultato = a/b;
+    return risultato;
 }
 
 int quoziente(int a, int b) {
-    // TODO Definire il corpo della funzione
-    return 0;
+    int risultato;
+    risultato = a/b;
+    return risultato;
 }
 
 float frazionale(int a, int b) {
-    // TODO Definire il corpo della funzione
-    return 0;
+    int q;
+    float d, risultato;
+    q = quoziente(a,b);
+    d = divisione((float)a, (float)b);
+    risultato = d - q;
+    return risultato;
 }
 
 float potenza(float base, int esponente) {
-    // TODO Definire il corpo della funzione
-    return 0;
+    float risultato;
+    risultato = 1;
+    while(esponente > 0) {
+        risultato *= base;
+        esponente--;
+    }
+    return risultato;
 }
